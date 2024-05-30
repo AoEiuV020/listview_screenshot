5725fae3968420561ccccc6616b9dcb3a808048c
AoEiuV020 <AoEiuV020@users.noreply.github.com>
2024-05-31 02:30:13 +0800

    deploy: c80e1912191e0418405640b0f737f3b1202afad9

---
+++ imageMergeTransform.dart
@@ -0,0 +1,8 @@
+// ignore_for_file: file_names
+
+import 'package:listview_screenshot/worker_main.dart';
+
+/// dart compile js imageMergeTransform.dart -o imageMergeTransform.js -O4
+void main() {
+  imageMergeTransformWorkerMain();
+}
