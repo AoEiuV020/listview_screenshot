9d4edb01f2cf5c1567c576a32aaad0564c106a8d
AoEiuV020 <aoeiuv020@gmail.com>
2024-11-09 18:10:39 +0800

    更新melos配置

--- script/env.sh
+++ script/env.sh
@@ -2,6 +2,7 @@
 set -e
 script_dir=$(cd $(dirname $0);pwd)
 ROOT=$(dirname "$script_dir")
+project_name=$(basename "$ROOT")
 apps_dir="$ROOT"/apps
 packages_dir="$ROOT"/packages
 organization=com.aoeiuv020.flutter.listviewscreenshot
--- script/init_melos.sh
+++ script/init_melos.sh
@@ -5,5 +5,7 @@
 if ! command -v melos > /dev/null; then
     echo "melos is not installed. Installing..."
     dart pub global activate melos
-    export PATH="$PATH":"$HOME/.pub-cache/bin"
+    if [[ ":$PATH:" != *":$HOME/.pub-cache/bin:"* ]]; then
+        export PATH="$PATH:$HOME/.pub-cache/bin"
+    fi
 fi
